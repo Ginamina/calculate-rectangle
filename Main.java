8ef91a6b4392cb4147bfc215a3d79d0dbe07a26c
kobajasimidori <65866791+Ginamina@users.noreply.github.com>
2021-07-08 20:21:57 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    printcalculateRectangleArea(2, 5);
+  }
+
+  public static void printcalculateRectangleArea(int width, int height){
+    int area=width * height;
+    System.out.println("A négyszög területe " + area + " négyzetméter.");
+  }  
+}
